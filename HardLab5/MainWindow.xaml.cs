799761f46ad665a90456441f7ea494877f2728f3
Elizaveta Pernikova <lisapernikowa@mail.ru>
2023-04-14 18:34:53 +0500

    ww

--- HardLab5/MainWindow.xaml.cs
+++ HardLab5/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Data.Common;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data.Common;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,15 +14,42 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.Collections.Generic;
 
 namespace HardLab5
 {
 
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void OpenDataFile(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog openFolderDialog = new FolderBrowserDialog();
+            string folderPath = "";
+
+            if (openFolderDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                folderPath = openFolderDialog.SelectedPath; 
+            }
+
+            string folderName = folderPath.Split('\\')[folderPath.Split('\\').Length - 1];
+
+            folderTree.Header = folderName;
+
+            foreach(string file in Directory.EnumerateFiles(folderPath))
+            {
+                folderTree.Items.Add(file.Split('\\')[(file.Split('\\').Length-1)]);
+            }
+
+        }
+
     }
+
+
 }
